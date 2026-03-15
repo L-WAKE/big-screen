001bb8b0d814198c883c668833d2f7b76513891d
奔跑的面条 <1262327911@qq.com>
2026-03-15 19:19:21 +0800

    feat: 新增 AI 提示入口

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -110,7 +110,10 @@
     <div class="go-login-box-footer">
       <layout-footer></layout-footer>
     </div>
+    <go-view-pro-ai></go-view-pro-ai>
   </div>
+  <!-- pro -->
+   <GoViewProAI></GoViewProAI>
 </template>
 
 <script lang="ts" setup>
@@ -122,10 +125,12 @@ import { GoThemeSelect } from '@/components/GoThemeSelect'
 import { GoLangSelect } from '@/components/GoLangSelect'
 import { LayoutHeader } from '@/layout/components/LayoutHeader'
 import { LayoutFooter } from '@/layout/components/LayoutFooter'
+import { GoViewProAI } from '@/components/Tips/GoViewProAI'
 import { PageEnum } from '@/enums/pageEnum'
 import { icon } from '@/plugins'
 import { StorageEnum } from '@/enums/storageEnum'
 import { routerTurnByName, cryptoEncode, setLocalStorage } from '@/utils'
+
 const { GO_LOGIN_INFO_STORE } = StorageEnum
 
 const { PersonOutlineIcon, LockClosedOutlineIcon } = icon.ionicons5
